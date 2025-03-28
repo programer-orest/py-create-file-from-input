a1c2bf791805cd4d1eb1ed7d53b6c95cf5f601d1
Orest Yakymiv <akimivorest@gmail.com>
2025-03-28 19:43:21 +0200

    create function for add the file with newline

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-def main():
-    # write your code here
-    pass
-
+def main() -> None:
+    name_of_file = input("Enter name of the file: ")
+    res = f"{name_of_file}.txt"
+    with open(res, "w") as file:
+        while True:
+            new_line = input("Enter new line of content: ")
+            if new_line.lower() == "stop":
+                break
+            file.write(new_line + "\n")
 
 if __name__ == "__main__":
     main()
